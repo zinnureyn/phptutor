c22fc2393a6d0275645e9bf58986531267095967
zinnureyn <m.HURRIYET65>
2023-02-05 14:43:24 +0300

    css and js added

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <title>Php Tutor</title>
 </head>
 
 <body>
 
 //add javascript file
-    <script src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
